24f824bfb6310fee5f77893eb3283cb7cbef328a
Dilbar <dibux9@gmail.com>
2022-08-16 12:43:23 -0400

    commit

---
+++ m3-lambda/src/main/java/com/cydeo/OrangeTest.java
@@ -0,0 +1,40 @@
+package com.cydeo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrangeTest {
+
+    public static void main(String[] args) {
+
+        List<Orange> inventory = new ArrayList<Orange>();
+
+        inventory.add(new Orange(300,Color.GREEN));
+        inventory.add(new Orange(100,Color.RED));
+        inventory.add(new Orange(200,Color.GREEN));
+        inventory.add(new Orange(50,Color.RED));
+
+        OrangeFormatter orangeLambda = orange -> "An orange of" + orange.getWeight() + "g";
+        prettyPrintOrange(inventory,orangeLambda);
+
+        //prettyPrintOrange(inventory, orange -> "An orange of" + orange.getWeight() + "g");
+
+
+        System.out.println("-----------------------------------------------------");
+
+        OrangeFormatter fancyFormatter = orange -> {    // more than one line need to use {}
+            String ch = orange.getWeight() > 200 ? "Heavy" : "Light";
+            return "A " + ch + " " + orange.getColor() + " orange";    //  more than one line so need to add return keyword
+        };
+
+        prettyPrintOrange(inventory, fancyFormatter);
+
+    }
+
+    private static void prettyPrintOrange(List<Orange> inventory, OrangeFormatter formatter){
+        for(Orange orange : inventory){
+            String output = formatter.accept(orange);
+            System.out.println(output);
+        }
+    }
+}

9f2d338b887a72351187e42d84b92fba2f1e66b3
Dilbar <dibux9@gmail.com>
2022-08-16 12:49:43 -0400

    commit

--- m3-lambda/src/main/java/com/cydeo/task/OrangeTest.java
+++ m3-lambda/src/main/java/com/cydeo/task/OrangeTest.java
@@ -1,4 +1,8 @@
-package com.cydeo;
+package com.cydeo.task;
+
+import com.cydeo.Color;
+import com.cydeo.task.Orange;
+import com.cydeo.task.OrangeFormatter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,7 +13,7 @@ public class OrangeTest {
 
         List<Orange> inventory = new ArrayList<Orange>();
 
-        inventory.add(new Orange(300,Color.GREEN));
+        inventory.add(new Orange(300, Color.GREEN));
         inventory.add(new Orange(100,Color.RED));
         inventory.add(new Orange(200,Color.GREEN));
         inventory.add(new Orange(50,Color.RED));

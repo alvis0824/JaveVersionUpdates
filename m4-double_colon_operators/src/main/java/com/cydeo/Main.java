9dcbe74c1d27dbd0b7aea9c8c8c72cd5211a719e
Dilbar <dibux9@gmail.com>
2022-08-18 12:49:19 -0400

    commit

--- m4-double_colon_operators/src/main/java/com/cydeo/Main.java
+++ m4-double_colon_operators/src/main/java/com/cydeo/Main.java
@@ -1,5 +1,9 @@
 package com.cydeo;
 
+import java.util.Arrays;
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class Main {
@@ -11,6 +15,28 @@ public class Main {
         Calculate sum = (x, y) -> System.out.println(x+y);
 
         Calculate s1 = (x, y) -> Calculator.findSum(x,y);
+
+        //Reference to a static method
+        Calculate s2 = Calculator::findSum;
+        s2.calculate(10,20);
+
+        //Reference to a instance method
+        Calculator obj = new Calculator();
+        Calculate s3 = obj::findMultiply;
+
+        Calculate s4 = new Calculator() :: findMultiply;
+
+        BiFunction<String, Integer, String> func =  (str,i) -> str.substring(i);
+
+        BiFunction<String, Integer, String> func2 = String::substring;
+        System.out.println(func.apply("abcdefg",2));
+
+        Function<Integer, Double> b = new MyClass()::method;
+        BiFunction<MyClass, Integer, Double> b1 = MyClass::method;   // if the first parameter we define the class, then we do not need to use new keyword to create an object
+
+        Consumer<Integer> display = i -> System.out.println(i);
+        Consumer<Integer> display2 = System.out::println;
+        display2.accept(3);
 
     }
 }

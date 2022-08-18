f10c9b813b4e29cd63be2c0e934d14f3fe0b03e7
Dilbar <dibux9@gmail.com>
2022-08-18 15:51:39 -0400

    commit

---
+++ m6-stream/src/main/java/com/cydeo/Course.java
@@ -0,0 +1,14 @@
+package com.cydeo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+
+public class Course {
+
+    private String courseName;
+    private int courseId;
+
+}
